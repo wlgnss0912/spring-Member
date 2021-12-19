52064394dc51339939b73dd08d5b0af253476725
JJH <jeonjihoon93@naver.com>
2021-12-19 10:10:05 +0900

    21.12.19 예제 1

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -9,14 +9,14 @@ public class MemberServiceTest {
 
     @Test
     void join(){
-        //given
+        //given-이런게 주어졌을 경우
         Member member = new Member(1L, "memberA", Grade.VIP);
 
-        //when
+        //when-이것을 했을때
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
 
-        //then
+        //then-이렇게된다
         Assertions.assertThat(member).isEqualTo(findMember);
     }
 
